3a9ae65fe03f5131c3af2eb7b0abe733b96ee411
ucho456 <watapqxyz@gmail.com>
2022-08-31 17:31:29 +0900

    インタフェース経由で実行可能にする

--- store/repository.go
+++ store/repository.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/ucho456/go_todo_app/clock"
 	"github.com/ucho456/go_todo_app/config"
 )
 
@@ -29,4 +30,37 @@ func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
 	xdb := sqlx.NewDb(db, "mysql")
 
 	return xdb, func() { _ = db.Close() }, nil
+}
+
+type Beginner interface {
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
+}
+
+type Preparer interface {
+	PreparexContext(ctx context.Context, query string) (*sqlx.Stmt, error)
+}
+
+type Execer interface {
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
+}
+
+type Queryer interface {
+	Preparer
+	QueryxContext(ctx context.Context, query string, args ...any) (*sqlx.Rows, error)
+	QueryRowxContext(ctx context.Context, query string, args ...any) *sqlx.Row
+	GetContext(ctx context.Context, dest interface{}, query string, args ...any) error
+	SelectContext(ctx context.Context, dest interface{}, query string, args ...any) error
+}
+
+var (
+	_ Beginner = (*sqlx.DB)(nil)
+	_ Preparer = (*sqlx.DB)(nil)
+	_ Queryer  = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.Tx)(nil)
+)
+
+type Repository struct {
+	Clocker clock.Clocker
 }
