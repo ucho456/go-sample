58df67f5bcca62ef119c34efc78b1de0cd1d6c51
ucho456 <watapqxyz@gmail.com>
2022-09-01 16:39:27 +0900

    DB保存に保存する処理をエンドポイントに実装

--- mux.go
+++ mux.go
@@ -1,28 +1,36 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator/v10"
+	"github.com/ucho456/go_todo_app/clock"
+	"github.com/ucho456/go_todo_app/config"
 	"github.com/ucho456/go_todo_app/handler"
 	"github.com/ucho456/go_todo_app/store"
 )
 
-func NewMux() http.Handler {
+func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), error) {
 	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
 
-	v := validator.New()
+	db, cleanup, err := store.New(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
+	}
 
-	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	r := &store.Repository{Clocker: clock.RealClocker{}}
+	v := validator.New()
+	at := &handler.AddTask{DB: db, Repo: r, Validator: v}
 	mux.Post("/tasks", at.ServeHTTP)
 
-	lt := &handler.ListTask{Store: store.Tasks}
+	lt := &handler.ListTask{DB: db, Repo: r}
 	mux.Get("/tasks", lt.ServeHTTP)
 
-	return mux
+	return mux, cleanup, nil
 }
